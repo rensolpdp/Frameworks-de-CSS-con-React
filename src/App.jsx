1a33c54514fdae072ac06304fbd166c4ac8205b0
Pistan Renso <rensolpdp@gmail.com>
2026-09-17 16:37:20 -0300

    Componente ProductCard completado con Bootsrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
 import styles from './App.module.css';
 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import ProductCard from './components/productcard/ProductCard';
+
 const turnosIniciales = [
   { id: 1, cliente: 'Nico Rios', servicio: 'Corte + Barba', precio: 11000, barbero: 'Pepe', notas: 'Cliente habitual' },
   { id: 2, cliente: 'Gabi Tolaba', servicio: 'Color', precio: 25000, barbero: 'Gallina', notas: 'Alergia a tintes fuertes' },
@@ -11,6 +17,11 @@ const turnosIniciales = [
   { id: 4, cliente: 'Gabi Tolaba', servicio: 'Corte de cabello', precio: 8000, barbero: null, notas: 'Atiende el primer barbero disponible' },
   { id: 5, cliente: 'Renso Pistan', servicio: 'Diseño de Cejas', precio: 3000, barbero: 'Gallina', notas: 'Primera vez' },
   { id: 6, cliente: 'Amaya Braian', servicio: 'Corte con diseño', precio: 15000, barbero: 'Pepe', notas: 'Diseño en V' }
+];
+
+const ProductosPrueba=[
+  {id:1, nombre: 'Pepito', precio: '10000',img:'https://imgs.search.brave.com/SRTNEBWebEZxViUbtmdw_SdirfecwOzfSia2R3ygzCY/rs:fit:860:0:0:0/g:ce/aHR0cDovL3NhbHRh/MjEuY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDA5LzA1L2pw/Z19yb2RvbGZvX2Fy/ZWRlc181LmpwZw', stock:10},
+  {id:2, nombre: 'Pepona', precio: '5500', img:'https://imgs.search.brave.com/S7h7Ans0DrGtdktEH3UFjrL_Ide8pLvNGntSgcmtB30/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzgxMzUxOC1NTEE4/NDM1MTA0MDY3NV8w/NTIwMjUtRS53ZWJw', stock:0}
 ];
 
 export default function App() {
@@ -25,15 +36,19 @@ export default function App() {
   return (
     <div className={styles.appContainer}>
       <Navbar />
-      <Main 
-        turnos={turnosFiltradosYOrdenados}
-        estadoPantalla={estadoPantalla}
-        filtroActual={filtroServicio}
-        ordenActual={orden}
-        cambiarFiltro={(e) => setFiltroServicio(e.target.value)}
-        ordenar={() => setOrden(orden === 'asc' ? 'desc' : 'asc')}
-        simularEstado={(nuevoEstado) => setEstadoPantalla(nuevoEstado)}
-      />
+
+      <div>
+        {ProductosPrueba.map((PRODUCTO)=>(
+          <ProductCard
+            key={PRODUCTO.id}
+            nombre={PRODUCTO.nombre}
+            precio={PRODUCTO.precio}
+            img={PRODUCTO.img}
+            stock={PRODUCTO.stock}
+            />
+        ))}
+      </div>
+
       <Footer />
     </div>
   );
--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-export default function ProductCard() {
+export default function ProductCard({nombre, precio, img, stock}) {
+
   return (
     <div>
-
-    <h1>Este es un Titulo</h1>
+   
+    <div className="card">
+      <img src={img} className="card-img-top" alt={nombre}/>
+      <div className="card-body">
+        <h5 className="card-title">{nombre}</h5>
+        <p className="card-text">DESCRIPCION</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Precio: {precio}</li>
+        <li className="list-group-item">Stock: {stock}</li>
+      </ul>
+      <div className="card-body">
+      <button href="#" className={stock ?'btn btn-primary':'btn btn-secondary'} disabled={!stock}> Agregar</button>
+      </div>
+    </div>
 
     </div>
   )
