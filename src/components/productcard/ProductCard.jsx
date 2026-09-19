d9a829d9940e143490def7cf7132a7eb36b949d2
Pistan Renso <rensolpdp@gmail.com>
2026-09-19 20:24:13 -0300

    Las cards funcionando con Tailwind de modder

--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -7,21 +7,18 @@ export default function ProductCard({ nombre, precio, imagen, stock }) {
 
   return (
     <div 
-      className={` flex flex-col gap-4 bg-white shadow-md rounded-2xl overflow-hidden m-2 relative w-64 ${sinStock ? 'opacity-75' : ''}`}
+      className={`flex flex-col justify-between bg-white shadow-md rounded-2xl overflow-hidden m-2 relative w-64  ${sinStock ? 'opacity-75' : ''}`}
     >
-      {/* Badge de Sin Stock */}
       {sinStock && (
         <span className="bg-red-500 text-white text-xs px-2 py-1 rounded absolute top-2 left-2 z-10">
           Sin stock
         </span>
       )}
 
-      {/* Imagen del producto */}
       {!imagenRota ? (
         <img
           src={imagen}
-        
-          className="w-full  object-cover"
+          className="w-full object-contain p-2" 
           alt={nombre}
           onError={() => setImagenRota(true)}
         />
@@ -31,25 +28,24 @@ export default function ProductCard({ nombre, precio, imagen, stock }) {
         </div>
       )}
 
-      {/* Cuerpo de la tarjeta */}
-      <div className="p-4 text-center">
-        <h5 className="text-lg font-semibold mb-1 text-gray-800">{nombre}</h5>
-        <p className="text-gray-500 font-bold text-xl mb-3">\${precioFormateado}</p>
-        
-        {/* Botón */}
-        <div className=''>
-        <button
-          className={`w-full py-2 rounded-lg font-semibold transition-colors ${
-            sinStock 
-              ? 'bg-gray-400 text-white cursor-not-allowed' 
-              : 'bg-yellow-500 hover:bg-yellow-600 text-gray-900'
-          }`}
-          disabled={sinStock}
-        >
-          {sinStock ? 'Sin stock' : 'Agregar'}
-        </button>
+      <div className="p-4 text-center flex flex-col justify-between flex-1">
+        <div>
+          <h5 className="text-sm font-semibold mb-1 text-gray-800 line-clamp-2">{nombre}</h5>
+          <p className="text-gray-500 font-bold text-lg mb-3">\${precioFormateado}</p>
         </div>
+        
+          <button 
+            className={`w-full py-2 px-4 rounded-lg font-semibold transition-colors ${
+              sinStock 
+                ? 'bg-gray-400 text-white cursor-not-allowed' 
+                : 'bg-yellow-500 hover:bg-yellow-600 text-gray-900'
+            }`}
+            disabled={sinStock}
+          >
+            {sinStock ? 'Sin stock' : 'Agregar'}
+          </button>
       </div>
     </div>
   );
 }
+
